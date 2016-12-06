ab51e50122454d881685998b32e0d7a7be2ec1cb
Avdhesh AKhani <avdhesh.akhani123@gmail.com>
2016-12-06 16:32:57 -0500

    Addes User Model Class and testCase

---
+++ app/src/test/java/com/firstdataproblem/model/UserTest.java
@@ -0,0 +1,31 @@
+package com.firstdataproblem.model;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Avdhesh AKhani on 12/6/2016.
+ */
+public class UserTest {
+
+    User u;
+    public UserTest(){
+        u = new User();
+    }
+
+    @Test
+    public void UserNameTest() {
+
+
+        u.setName("foo");
+
+        Assert.assertEquals("foo", u.getName());
+    }
+    @Test
+    public void failingTest(){
+        Assert.assertFalse(true);
+    }
+
+}
